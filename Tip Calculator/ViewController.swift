00bd86211cfbe22d1d9ebb8bac63628bc88932d2
Iqra Abbaali <iqraabbaali@iqras-macbook-pro.local>
2019-03-27 17:14:04 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Iqra Abbaali on 3/27/19.
+//  Copyright © 2019 Iqra Abbaali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
